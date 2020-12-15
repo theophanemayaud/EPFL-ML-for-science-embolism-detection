18a0238dc62dd3e1da148c4e4f00e3286d600bd4
ATchelet <58084722+ATchelet@users.noreply.github.com>
2020-12-15 21:48:25 +0200

    run.py works

--- run.py
+++ run.py
@@ -1,19 +1,20 @@
 # import libraries
-import glob, os, ntpath, sys
+import glob, os, ntpath, sys, pathlib
 import numpy as np
 import pandas as pd
 import cv2 as cv
 import torch
+import torch.nn as nn
 import xml.etree.ElementTree as ET
 from scipy.ndimage import label as region_map
 
 def main():
     # create a dictionary for the CSV 
-    csv_dict = {'plant_name'=[], 'slice'=[], 'vessle_num'=[], 'surface'=[], 'diameter'=[], 'x-coordinate'=[], 'y-coordinate'=[]}
+    csv_dict = {'plant_name':[], 'slice':[], 'vessle_num':[], 'surface':[], 'diameter':[], 'x-coordinate':[], 'y-coordinate':[]}
 
     # list all XML files to get plant names and voxel sizes
-    xml_paths_l = sorted(glob.glob(living_plants_path+'**\\*.xml))
-    xml_paths_f = sorted(glob.glob(flushed_plants_path+'**\\*.xml))
+    xml_paths_l = sorted(glob.glob(path_l+'**\\*.xml'))
+    xml_paths_f = sorted(glob.glob(path_f+'**\\*.xml'))
 
     # set device
     if not torch.cuda.is_available():
@@ -30,27 +31,27 @@ def main():
         plant_name = os.path.basename(os.path.dirname(xml_path)) # remove file name then keep only the directory name
         path = pathlib.Path('.\\output\\living\\'+plant_name) # set plant output directory
         path.mkdir(parents=True, exist_ok=True) # creates folder if doesn't exist
-        for img_path in np.array(sorted(glob.glob(living_plants_path+f'**{plant_name}\\*'+type)),dtype=object) # go through all sacns in folder
-            slice = extract_name(img_path) # extract slice name
+        for img_path in np.array(sorted(glob.glob(path_l+f'**{plant_name}\\*'+img_type)),dtype=object): # go through all sacns in folder
+            slice_name = extract_name(img_path) # extract slice name
             img = cv.imread(img_path, cv.IMREAD_UNCHANGED)/(0xFFFF) # load image
             pred = out_predict(model, img, device) # predict embolized area
 
             # store preicted image overlayed on top of original image
-            pred_ol = np.repeat(img[:, :, np.newaxis], 3, axis=2).astype(np.uint16) # convert image from grayscale to RGB
+            pred_ol = np.repeat(img[:, :, np.newaxis], 3, axis=2) # convert image from grayscale to RGB
             pred_ol[pred==1] = np.array([0xFFFF,0,0]) # overlay prediction over original image
-            cv.imwrite(f'.\\output\\living\\{plant_name}\\{slice}{type}', pred_ol) # store overlayed image
+            cv.imwrite(f'.\\output\\living\\{plant_name}\\{slice_name}{img_type}', pred_ol) # store overlayed image
 
             # analyze embolized areas
             vessels_map, vessels = region_map(pred) # create a regions map for the embolized areas
             for vessel in range(vessels): # run for each area
-                vessel_pixels = np.argwhere(vessels_map==vessel) # an array of what pixels contain the vessel
+                vessel_pixels = np.argwhere(vessels_map==vessel+1) # an array of what pixels contain the vessel
                 surface = voxel_size**2 * len(vessel_pixels) # calculate the surface area of the vessel in mm^2 
                 diameter = np.sqrt(4 * surface / np.pi) # calculate the equivalent circle diameter in mm
-                yx = voxel_size * (np.max(vessel_pixels, axis=0) - np.max(vessel_pixels, axis=0)) / 2 # calculate equivalent circle centre coordinate in mm
+                yx = voxel_size * (np.max(vessel_pixels, axis=0) + np.min(vessel_pixels, axis=0)) / 2 # calculate equivalent circle centre coordinate in mm
 
                 # store all data into dictionary
                 csv_dict['plant_name'].append(plant_name)
-                csv_dict['slice'].append(slice)
+                csv_dict['slice'].append(slice_name)
                 csv_dict['vessle_num'].append(vessel)
                 csv_dict['surface'].append(surface)
                 csv_dict['diameter'].append(diameter)
@@ -58,32 +59,32 @@ def main():
                 csv_dict['y-coordinate'].append(yx[0])
 
     # run prediction and analysis for all flushed plants images in subdirectories
-    for xml_path in xml_paths_l:
+    for xml_path in xml_paths_f:
         voxel_size = extract_voxel(xml_path) # extract the voxel size of the scans
         plant_name = os.path.basename(os.path.dirname(xml_path)) # remove file name then keep only the directory name
         path = pathlib.Path('.\\output\\flushed\\'+plant_name) # set plant output directory
         path.mkdir(parents=True, exist_ok=True) # creates folder if doesn't exist
-        for img_path in np.array(sorted(glob.glob(flushed_plants_path+f'**{plant_name}\\*'+type)),dtype=object) # go through all sacns in folder
-            slice = extract_name(img_path) # extract slice name
+        for img_path in np.array(sorted(glob.glob(path_f+f'**{plant_name}\\*'+img_type)),dtype=object): # go through all sacns in folder
+            slice_name = extract_name(img_path) # extract slice name
             img = cv.imread(img_path, cv.IMREAD_UNCHANGED)/(0xFFFF) # load image
             pred = out_predict(model, img, device) # predict embolized area
 
             # store preicted image overlayed on top of original image
-            pred_ol = np.repeat(img[:, :, np.newaxis], 3, axis=2).astype(np.uint16) # convert image from grayscale to RGB
+            pred_ol = np.repeat(img[:, :, np.newaxis], 3, axis=2) # convert image from grayscale to RGB
             pred_ol[pred==1] = np.array([0xFFFF,0,0]) # overlay prediction over original image
-            cv.imwrite(f'.\\output\\flushed\\{plant_name}\\{slice}{type}', pred_ol) # store overlayed image
+            cv.imwrite(f'.\\output\\flushed\\{plant_name}\\{slice_name}{img_type}', pred_ol) # store overlayed image
 
             # analyze embolized areas
             vessels_map, vessels = region_map(pred) # create a regions map for the embolized areas
             for vessel in range(vessels): # run for each area
-                vessel_pixels = np.argwhere(vessels_map==vessel) # an array of what pixels contain the vessel
+                vessel_pixels = np.argwhere(vessels_map==vessel+1) # an array of what pixels contain the vessel
                 surface = voxel_size**2 * len(vessel_pixels) # calculate the surface area of the vessel in mm^2 
                 diameter = np.sqrt(4 * surface / np.pi) # calculate the equivalent circle diameter in mm
-                yx = voxel_size * (np.max(vessel_pixels, axis=0) - np.max(vessel_pixels, axis=0)) / 2 # calculate equivalent circle centre coordinate in mm
+                yx = voxel_size * (np.max(vessel_pixels, axis=0) + np.min(vessel_pixels, axis=0)) / 2 # calculate equivalent circle centre coordinate in mm
 
                 # store all data into dictionary
                 csv_dict['plant_name'].append(plant_name+'_dry')
-                csv_dict['slice'].append(slice)
+                csv_dict['slice'].append(slice_name)
                 csv_dict['vessle_num'].append(vessel)
                 csv_dict['surface'].append(surface)
                 csv_dict['diameter'].append(diameter)
@@ -91,7 +92,8 @@ def main():
                 csv_dict['y-coordinate'].append(yx[0])
 
     # create CSV from dictionary
-    pd.to_csv('.\\output\\analyzed_data.csv', pd.DataFrame.from_dict(csv_dict))
+    pd.DataFrame.from_dict(csv_dict).to_csv('.\\output\\analyzed_data.csv', index=False)
+
 
 #############################
 # Command line Help methods #
@@ -99,23 +101,33 @@ def main():
 
 def help():
     help_s = '''
-        usage: pipeline.py living_plants_path flushed_plants_path [options] ...
-    
-        This is a tool to detect and analyze the ratio between embolism regions in living and flushed plants. The tool requires seperate paths to the top directories containing all the living and flushed plant images
-        
-        living_plants_path      A path to the top directory of all living plants images.
-        flushed_plants_path     A path to the top directory of all flushed plants images.
-        **Note**: By default the tool will analyze all .TIF files in the subdirectories from the paths, but the type could be modified by [-type <type>]
-        
-        Options:
-            -h, -help, --h, --help  Displays this page. Shows instructions on how to use the tool and its different options.
-            -model <models_path>    Modifies the location of the models. By defult models folder is expected at .\models. 
-                                    The names of the model files need to be model._l.pickle and model_f.pickle for the living plants and flushed plants respectively.
-            -type <img_type>        Modifies type of the images used. By defult the type is TIF.
-        
-        Example:
-            pipeline.py living_plants_path flushed_plants_path -model ./models/ -type PNG
+*****************************************
 
+usage: pipeline.py living_plants_path flushed_plants_path [options] ...
+
+*****************************************
+
+This is a tool to detect and analyze the ratio between embolism regions in living and flushed plants. The tool requires seperate paths to the top directories containing all the living and flushed plant images
+
+living_plants_path      A path to the top directory of all living plants images.
+flushed_plants_path     A path to the top directory of all flushed plants images.
+**Note**: By default the tool will analyze all .TIF files in the subdirectories from the paths, but the type could be modified by [-type <type>]
+
+*****************************************
+
+Options:
+    -h, -help, --h, --help  Displays this page. Shows instructions on how to use the tool and its different options.
+    -model <models_path>    Modifies the location of the models. By defult models folder is expected at .\models. 
+                            The names of the model files need to be model._l.pickle and model_f.pickle for the living plants and flushed plants respectively.
+    -type <img_type>        Modifies type of the images used. By defult the type is TIF.
+
+*****************************************
+
+Example:
+    pipeline.py living_plants_path flushed_plants_path -model ./models/ -type PNG
+
+
+*****************************************
         '''
     print(help_s)
 
@@ -139,7 +151,7 @@ def extract_voxel(xml_path):
     root = tree.getroot() # get the root of the XML tree
     voxel_size = root[0][1][0].attrib['X'] # get <conebeam\volume_acquisition\voxelSize['X']>
 
-    return voxel_size
+    return float(voxel_size)
 
 
 # A method to exrtact just the file name from a path
@@ -238,6 +250,112 @@ def out_predict(model, img, device, in_size=572, out_size=388, extend=True):
         pred_max_logit[:,start[0]:start[0]+out_size, start[1]:start[1]+out_size] = np.fmax(prediction,pred_max_logit[:,start[0]:start[0]+out_size, start[1]:start[1]+out_size]) # check for class max lieklihood for each overlapping pixel
         pred[start[0]:start[0]+out_size, start[1]:start[1]+out_size] = np.argmax(pred_max_logit[:,start[0]:start[0]+out_size, start[1]:start[1]+out_size],axis=0) # determine class of segment's pixels
     return pred # convert to np.array and return
+
+
+################################################
+# U_NET pytorch architecture and subcomponents #
+################################################
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling and conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up_conv = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            nn.ZeroPad2d((0,1,0,1)),
+            nn.Conv2d(in_channels, out_channels, kernel_size=2)
+        )
+        # self.up_conv = nn.ConvTranspose2d(in_channels , out_channels, kernel_size=2, stride=2)
+        self.double_conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        # up-conv input
+        x1 = self.up_conv(x1)
+
+        # crop and contatinate
+        diffY = int((x2.size()[2] - x1.size()[2]) // 2)
+        diffX = int((x2.size()[3] - x1.size()[3]) // 2)
+        x2 = x2[:,:,diffY:-diffY, diffX:-diffX]
+        x = torch.cat([x2, x1], dim=1)
+        return self.double_conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class U_net(nn.Module):
+    def __init__(self):
+        super(U_net, self).__init__()
+
+        self.inc = DoubleConv(1, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
+        self.out = OutConv(64, 2)
+        self.init_wb()
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.out(x)
+        return logits
+
+    def init_wb(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=np.sqrt(2/np.prod(m.weight.shape[1:])))
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
+
 
 ########################
 # Command line handler #
